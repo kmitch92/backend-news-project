e0270ef20ab2c3678bb7e0982029bd3973356de4
kiel <kielmitchell8@gmail.com>
2022-08-01 21:05:23 +0100

    complete first two tests of ticket 5. Requires: complete third currently written test, further tests for patch specific behaviour, refactor for cleaning up destructuring

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,7 +12,7 @@ afterAll(() => {
   return db.end();
 });
 
-describe.only("ALL /*", () => {
+describe("ALL /*", () => {
   test("status: 404 for endpoint not found", () => {
     return request(app)
       .get("/non_existent_endpoint")
@@ -23,7 +23,7 @@ describe.only("ALL /*", () => {
   });
 });
 
-describe.only("GET /api/topics", () => {
+describe("GET /api/topics", () => {
   test("should respond with status code:200, and with an array of all topics in the topics table of the database, with each item containing a 'slug' and a 'description' property.", () => {
     return request(app)
       .get("/api/topics")
@@ -39,7 +39,7 @@ describe.only("GET /api/topics", () => {
   });
 });
 
-describe.only("GET /api/articles/:article_id", () => {
+describe("GET /api/articles/:article_id", () => {
   test("should respond with a status code:200, and with an article object, containing the properties: author (matches the username from the users table), title, article_id, body, topic, created_at, votes", () => {
     return request(app)
       .get("/api/articles/1")
@@ -75,4 +75,43 @@ describe.only("GET /api/articles/:article_id", () => {
         expect(body.msg).toBe("Article Not Found");
       });
   });
+});
+
+describe.only("PATCH /api/articles/:article_id", () => {
+  test("should respond with status code:200 and the updated article when a successful patch request is made. This endpoint increments the votes on the article: the votes should be incremented by the passed amount", () => {
+    const voteIncrementer = { inc_votes: 7 };
+
+    return request(app)
+      .patch("/api/articles/2")
+      .send(voteIncrementer)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.updatedArticle.votes).toEqual(7);
+      });
+  });
+
+  test("should respond with a status code:400 when passed an invalid article_id type ", () => {
+    const voteIncrementer = { inc_votes: 7 };
+
+    return request(app)
+      .patch("/api/articles/two")
+      .send(voteIncrementer)
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Invalid ID Type");
+      });
+  });
+
+  //   test("should respond with a status: 404 when passed a valid but non-existent id", () => {
+  //     const voteIncrementer = { inc_votes: 7 };
+
+  //     return request(app)
+  //       .patch("/api/articles/200")
+  //       .send(voteIncrementer)
+  //       .expect(404)
+  //       .then(({ body }) => {
+  //         console.log(body);
+  //         expect(body.msg).toBe("Article Not Found");
+  //       });
+  //   });
 });
--- models/model.js
+++ models/model.js
@@ -18,3 +18,42 @@ exports.fetchArticleById = async (id) => {
     return result[0];
   }
 };
+
+exports.updateArticleById = async (articleId, newVoteInfo) => {
+  const newVote = newVoteInfo.inc_votes;
+
+  console.log(newVote);
+
+  const articleBeforeUpdate = await db.query(
+    "SELECT * FROM articles WHERE article_id = $1;",
+    [articleId.article_id]
+  );
+
+  //   let articleBeforeUpdate;
+
+  //   try {
+  //     articleBeforeUpdate = await db.query(
+  //       "SELECT * FROM articles WHERE article_id = $1;",
+  //       [articleId.article_id]
+  //     );
+  //   } catch {
+  //     console.log(articleBeforeUpdate);
+  //     return Promise.reject({ status: 404, msg: "Article Not Found" });
+  //   }
+
+  console.log("32", articleBeforeUpdate.rows[0].votes);
+
+  const currentVotes = articleBeforeUpdate.rows[0].votes;
+  const updatedVotes = currentVotes + newVote;
+
+  const { rows: result } = await db.query(
+    "UPDATE articles SET votes = $1 WHERE article_id = $2 RETURNING *;",
+    [updatedVotes, articleId.article_id]
+  );
+  console.log(result);
+  if (result.length < 1) {
+    return Promise.reject({ status: 404, msg: "Article Not Found" });
+  } else {
+    return result[0];
+  }
+};
